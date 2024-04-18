ed1e691a7e36fc83f52ec609a352737434d48330
mathiasaertsens <math.aertsens@gmail.com>
2024-04-18 22:54:58 +0200

    Creation role

---
+++ database/migrations/2024_04_18_205126_add_role_id_to_users_table.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Ajout du champ role_id après la colonne 'password'
+            $table->foreignId('role_id')->after('password')->nullable()->constrained('roles')->onDelete('set null');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
+        });
+    }
+};
