bb614d3b7bcd7249faffaf47b7465bdf7dd7e373
mathiasaertsens <math.aertsens@gmail.com>
2024-05-07 00:44:51 +0200

    add controllers for about page and home page + view about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="container mx-auto">
+        Fredhousedurbuy se situe dans le petit village de Heyd en bordure de forêt.
+        À 8 km de Durbuy petite ville bien connue.
+        A 3 km de Weris reconnue pour ses mégalithes.
+        A 43 km du circuit Spa-Francorchamps.
+
+        Fredhousedurbuy vous propose deux chambres d’hôtes à l’étage avec entrée indépendante .
+        Chacune sa salle de bain douche évier wc.
+        Télévision avec connexion wi-fi, Netflix YouTube…..
+        Petit espace commun avec frigo machine à thé machine à café, vins à disposition…
+
+        Vous pourrez profiter d’un grand jardin aménagé, d’une belle terrasse et d’un jacuzzi.
+        Deux chevaux dans la propriété en guise de compagnie reposante ainsi qu’un chat et de nombreux oiseaux en liberté.
+
+        Lors des repas, petit déjeuner et/ou repas du soir, possibilité de table d’hôtes, vous serez dans un espace intérieur cosy, déco fifties avec feu ouvert.
+
+        Délicieux restaurants et nombreuses promenades dans les forêts ardennaises.
+
+        Mon but est vous que vous passiez un moment agréable dans un cadre verdoyant calme harmonieux et que vous soyez choyé.
+        Accompagné de bons moments culinaires dans une ambiance festive et agréable.
+
+        Au plaisir de faire votre connaissance, à très vite 😊👍
+    </div>
+
+@endsection
