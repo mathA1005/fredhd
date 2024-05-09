c01b8b12f62eec845211ae4e4c7a43c04477339e
mathiasaertsens <math.aertsens@gmail.com>
2024-05-09 16:15:33 +0200

    web

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
-use App\Http\Controllers\ChambreController;
-use App\Http\Controllers\EquipementController;
-use App\Http\Controllers\ProfileController;
+
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\RoomOptionsController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReservationController;
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Correction pour utiliser les méthodes du contrôleur ChambreController
-Route::get('/chambre', [ChambreController::class, 'index'])->name('chambre.index');
-Route::get('/chambre/create', [ChambreController::class, 'create'])->name('chambre.create');
-Route::get('/chambre/{id}', [ChambreController::class, 'show'])->name('chambre.show');
-Route::post('/chambre', [ChambreController::class, 'store'])->name('chambre.store');
-Route::get('/equipements', [EquipementController::class, 'index'])->name('equipements.index');
-Route::get('/equipements/create', [EquipementController::class, 'create'])->name('equipements.create');
-Route::post('/equipements', [EquipementController::class, 'store'])->name('equipements.store');
-Route::get('/chambres/{chambre}/availability', [ChambreController::class, 'checkAvailability'])->name('chambres.checkAvailability');
-
-
-
 Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
-    Route::get('/reservations', [ReservationController::class, 'index'])->name('Admin.index');
+    Route::get('/reservations', [ReservationController::class, 'index'])->name('admin.index');
     // Ajoutez d'autres routes administratives ici
 });
 
@@ -37,6 +24,23 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+
+// Correction pour utiliser les méthodes du contrôleur ChambreController
+Route::get('/chambre', [RoomController::class, 'index'])->name('chambre.index');
+Route::get('/chambre/create', [RoomController::class, 'create'])->name('chambre.create');
+Route::get('/chambre/{id}', [RoomController::class, 'show'])->name('chambre.show');
+Route::post('/chambre', [RoomController::class, 'store'])->name('chambre.store');
+Route::get('/chambres/{chambre}/availability', [RoomController::class, 'checkAvailability'])->name('chambres.checkAvailability');
+
+Route::get('/equipements', [RoomOptionsController::class, 'index'])->name('equipements.index');
+Route::get('/equipements/create', [RoomOptionsController::class, 'create'])->name('equipements.create');
+Route::post('/equipements', [RoomOptionsController::class, 'store'])->name('equipements.store');
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 
 // Auth routes
 require __DIR__.'/auth.php';
